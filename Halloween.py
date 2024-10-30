9f984fab2576cad9e09806595e9c5f6ab76c2956
IdkMiguel <pruittmiguel5@gmail.com>
2024-10-30 13:03:48 -0400

    Update Halloween.py

--- Halloween.py
+++ Halloween.py
@@ -17,10 +17,10 @@ orange = (201, 75, 0)
 blood = (117,18,0)
 joker = (94,6, 143)
 joke = (0,133,0)
-ora = (136, 62, 0)
+ora = (136, 32, 0)
 white = (255,255,255)
 
-color_list = [joke, joker, ora]
+color_list = [joke, joker, ora, blood]
 def fade_in(color):
     red_rat = color[0] / 50
     red_org = color[0]
@@ -65,7 +65,7 @@ def lightning(color):
         np.show()
         time.sleep(random.randint(3,5)/100)
 
-def sparkl(back = purple, rg = joker, delay = 0.7, spark = 4):
+def sparkl(back = red, rg = ora, delay = 0.7, spark = 4):
     for i in range(spark):
         what = random.randint(0,29)
         who = random.randint(0,29)
@@ -76,13 +76,14 @@ def sparkl(back = purple, rg = joker, delay = 0.7, spark = 4):
         np[why] = rg
         np.show()
         time.sleep(delay)
+
 def chase(color, hold = 0.1):
     count = 0
     for i in range(np.n):
         np.fill(color)
     for i in range(np.n):
         if (i + count) % 3 == 0:
-            np[i] = (0,0,0)
+            np[i] = ( 0,0,0)
                 
         time.sleep(hold)
         np.show()
@@ -91,10 +92,24 @@ def chase(color, hold = 0.1):
 
 
 
+def fire(bck,flick,flick2,speed,t1):
+    np.fill(bck)
+    np.show()
+    for i in range(t1):
+        np[random.randint(0,np.n-2)] = flick
+        np.show()
+        time.sleep(speed)
+        np[random.randint(0,np.n-2)] = flick2
+        np.show()
+        time.sleep(speed)
+
 while True:
     rando = random.choice(color_list)
     chase(rando)
-    fadeout(joker)
-    fade_in(orange)
-    sparkl()
+    fadeout(rando)
+    fade_in(rando)
     lightning(rando)
+    fire((rando),(rando), (rando),0.03,.4)
+    sparkl()
+    chase(rando)
+    
